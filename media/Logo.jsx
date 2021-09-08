e5b0f26006cb2c748beff8338a16c21b3be8ab37
Marie-Claire Renier <MC@MacBook-Air-de-Marie-Claire.local>
2021-09-08 13:43:25 +0200

    Adjustments pictures weight + size + logo

--- media/Logo.jsx
+++ media/Logo.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import logo from './logo-nobg.png'
+import logo from './logo-nobg-200x144.png'
 import Image from 'next/image'
 import styles from '../styles/atoms/logo.module.scss'
 
 export default function Logo() {
 	return (
 		<div className={styles.logo}>
-			<Image src={logo} alt='Logo' priority />
+			<Image src={logo} alt='Logo' width={100} height={72} layout='fixed' priority />
 		</div>
 	)
 }
