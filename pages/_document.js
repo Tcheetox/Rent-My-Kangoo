82036e0812a2a761c4a0d6b5dc7ebefeb65ddc1f
KRE <tcheetox@gmail.com>
2021-09-20 18:43:00 +0200

    Attempt at optimizing font 2

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ export default class MyDocument extends Document {
 					<link rel='dns-prefetch preconnect' href='https://maps.gstatic.com' />
 					<link rel='dns-prefetch preconnect' href='https://maps.googleapis.com' />
 					<link rel='dns-prefetch preconnect' href='https://fonts.googleapis.com' />
-					<link href='https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;1,400&display=swap' rel='stylesheet' />
+					{/* <link href='https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;1,400&display=swap' rel='stylesheet' /> */}
 				</Head>
 				<body>
 					<Main />
